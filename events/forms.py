e13f3fe21adb66f7c13737f7cac570a13f65f6a1
Nikolay Slavev <nikolaislavev96@gmail.com>
2019-02-27 00:21:56 +0000

    Changes to client and server

--- events/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Author, NewsStory
-
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = NewsStory
-        fields = ('headline', 'category', 'region', 'details')
--- events/views.py
+++ events/views.py
@@ -2,39 +2,34 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Author, NewsStory
-from .forms import PostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
-import json
-import datetime
+import json, datetime
 
 
 @csrf_exempt
 def handle_login(request):
 	if request.method == 'POST':
-		print(1)
-		print(request.POST)
 		username = request.POST['username']
 		password = request.POST['password']
-		data = 'username = ' + username + ', password = ' + password
+		data = "username=%s, password=%s" % (username, password)
 		user = authenticate(request, username=username, password=password)
-	if user is not None:
-		print(2)
-		if user.is_active:
-			print(3)
-			login(request, user)
-			if (user.is_authenticated):
-				print(4)
-				print(user.username + ' is logged in')
-			return HttpResponse(data)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				if user.is_authenticated:
+					data = 'Hi ' + str(username) + '\nYou logged in with the following credentials: \n' + str(data)
+					return HttpResponse(data, status = 200)
+				else:
+					return HttpResponse("USER IS NEWW WASNT LOGGED IN!!!", status = 200)
+					#???????????????????????????????????????
+			else:
+				return HttpResponse('Invalid login', status = 401)
 		else:
-			print(5)
-			return HttpResponse('disabled account')
+			return HttpResponse('Invalid login', status = 401)
 	else:
-		print(6)
-		return HttpResponse('invalid login ' + data)
+		return HttpResponse('Requires a POST request', status = 503)
 
 @csrf_exempt
 def handle_logout(request):
@@ -45,12 +40,7 @@ def handle_logout(request):
 		else:
 			return HttpResponse("User is not logged in")
 	else:
-		return HttpResponse("Requires a POST request")
-
-		
-#def post_list(request):
-#	posts = NewsStory.objects.filter(date__lte=timezone.now()).order_by('date')
-#	return render(request, 'blog/post_list.html', {'posts':posts})
+		return HttpResponse("Requires a POST request", status = 503)
 
 @csrf_exempt
 def post_list(request):
@@ -58,99 +48,86 @@ def post_list(request):
 		body_unicode = request.body.decode("utf-8")
 		body_data = json.loads(body_unicode)
 		
+		posts = NewsStory.objects.all()
 		if 'category' not in body_data:
 			category = '*'
 		else:
-			category = body_data["category"]
-			
+			if body_data["category"] not in ["POL", "ART", "TECH", "TRIVIA"]:
+				return HttpResponse("Invalid Category. Must be POL, ART, TECH or TRIVIA", status = 503)
+			posts = posts.filter(category = body_data["category"])
 		if 'region' not in body_data:
 			region = '*'
 		else:
-			region = body_data["region"]
-			
+			if body_data["region"] not in ["UK", "EU", "W"]:
+				return HttpResponse("Invalid Region. Must be UK, EU or W", status = 503)
+			posts = posts.filter(region = body_data["region"])
 		if 'date' not in body_data:
 			date = '*'
 		else:
-			date_string = body_data["date"]
-			date_array = date_string.split("/")
+			if "/" not in body_data["date"] or len(body_data["date"].split("/")) !=3:
+				return HttpResponse("Invalid date format. Must be day/month/year", status=503)
+			date_array = body_data["date"].split("/")
+			if not date_array[0].isdigit() or not date_array[1].isdigit() or not date_array[2].isdigit():
+				return HttpResponse("Date inputs need to be integer", status=503)
 			day = int(date_array[0])
 			month = int(date_array[1])
 			year = int(date_array[2])
-			date = datetime.datetime(year, month, day)
-				
-		posts = NewsStory.objects.all()
-		
-		if category is not '*':
-			posts = posts.filter(category = category)
-		
-		if region is not '*':
-			posts = posts.filter(region = region)
-		
-		if date is not '*':
+			try:
+				date = datetime.datetime(year, month, day)
+			except Exception as e:
+				return HttpResponse(e, status=503)
 			posts = posts.filter(date__gte = date)
-		
+
 		data = list(posts.values())
 		return JsonResponse(data, safe=False)
 	else:
-		return HttpResponse("Requires a POST request")
-
+		return HttpResponse("Requires a POST request", status = 503)
 	
 @csrf_exempt
 def post_remove(request):
-	print(99999999999999999999)
-	print(request.POST)
-	
-	
 	if request.method == 'POST':
 		if (request.user.is_authenticated):
 			body_unicode = request.body.decode("utf-8")
 			body_data = json.loads(body_unicode)
-			
 			if 'story_key' not in body_data:
-				return HttpResponse("Story key required")
-			else:
-				story_key = body_data["story_key"]
-			print(story_key)
+				return HttpResponse("Story key required", status = 503)
+			story_key = body_data["story_key"]
 			post = NewsStory.objects.all()
 			post = post.filter(pk = story_key)
-			print(post)
 			post.delete()	
-			return HttpResponse("OK")
+			return HttpResponse("Story removed successfully")
 		else:
-			return HttpResponse("User is not logged in")
+			return HttpResponse("User is not logged in", status = 503)
 	else:
-		return HttpResponse("Requires a POST request")
-	
+		return HttpResponse("Requires a POST request", status = 503)
 		
 @csrf_exempt
 def post_new(request):
-	print(123)
 	if request.method == 'POST':
-		#form = PostForm(request.POST)
 		body_unicode = request.body.decode("utf-8")
 		body_data = json.loads(body_unicode)
-		if (request.user.is_authenticated):
-		
+		if request.user.is_authenticated:
 			headline = body_data["headline"]
 			details = body_data["details"]
 			category = body_data["category"]
 			region = body_data["region"]
-			#if form.is_valid():
-			#post = form.save(commit=False)
 			author = Author.objects.get(username=request.user)
+			if len(headline) > 64:
+				return HttpResponse("Exceeded Headline length of 64 characters", status = 503)
+			if len(details) > 512:
+				return HttpResponse("Exceeded Details length of 512 characters", status = 503)
+			if region not in ["UK", "EU", "W"]:
+				return HttpResponse("Invalid Region. Must be UK, EU or W", status = 503)
+			if category not in ["POL", "ART", "TECH", "TRIVIA"]:
+				return HttpResponse("Invalid Category. Must be POL, ART, TECH or TRIVIA", status = 503)
 			
 			new_post = NewsStory(author = author, headline = headline, category = category, region = region, details = details, date = timezone.now())
 			new_post.save()
-			#post.author = author
-			#post.date = timezone.now()
-			#post.save()
-			return HttpResponse("Created")
-			#else:
-				#return HttpResponse("Form is invalid")
+			return HttpResponse("Created Successfully")
 		else:
-			return HttpResponse("User not logged in")
+			return HttpResponse("User not logged in", status = 503)
 	else:
-		return HttpResponse("Requires a POST request")
+		return HttpResponse("Requires a POST request", status = 503)
 
 		
 		
